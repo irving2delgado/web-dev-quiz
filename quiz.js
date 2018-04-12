425dee513101ee6fabdf5904387fa92c79c08cec
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-04-12 16:24:29 -0600

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,8 +1,16 @@
-var submitButton = document.getElementById("submit");
+// var submitButton = document.getElementById("submit");
 
-submitButton.addEventListener("click", sayHello);
+// submitButton.addEventListener("click", sayHello);
 
-function sayHello() {
-  var messageBox = document.getElementById("message");
-  messageBox.innerText = "Hello there, this is a message!";
-}
+// function sayHello() {
+//   var messageBox = document.getElementById("message");
+//   messageBox.innerText = "Hello there, this is a message!";
+// }
+
+ function correctAnsw(){
+    var answer = parseFloat(document.getElementById("answerbox").value);
+   if   (answer == 1) {
+     document.getElementById("message").innerHTML = "correct!!";
+ }
+   
+ }
